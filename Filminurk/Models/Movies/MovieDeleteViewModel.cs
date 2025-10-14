07cc82ef86854bb3053de6e76fbee5f9d21537e7
Opilane TTHK <opilane@tthk.ee>
2025-10-14 14:13:48 +0300

    delete model
    not finished

---
+++ Filminurk/Models/Movies/MovieDeleteViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace Filminurk.Models.Movies
+{
+    public class MovieDeleteViewModel
+    {
+        public Guid? ID { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public DateOnly? FirstPublished { get; set; }
+        public string? Director { get; set; }
+        public List<string>? Actors { get; set; }
+        public decimal? CurrentRating { get; set; }
+
+
+        //enda valitud andmed
+
+        public int? Profit { get; set; }
+        public string? Awards { get; set; }
+        public string? AwardsDescription { get; set; }
+
+        //andmebaasi jaoks vajalikud
+        public DateTime? EntryCreatedAt { get; set; }
+        public DateTime? EntryModifiedAt { get; set; }
+    }
+}
