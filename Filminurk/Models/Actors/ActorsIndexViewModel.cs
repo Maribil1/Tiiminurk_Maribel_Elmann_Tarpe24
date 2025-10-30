60f82b34f60009f5ec6df5906e934191c3eeb57a
Opilane TTHK <opilane@tthk.ee>
2025-10-30 09:59:21 +0200

    ActorsIndexViewModel
    in views Actors

---
+++ Filminurk/Models/Actors/ActorsIndexViewModel.cs
@@ -0,0 +1,17 @@
+﻿using static Filminurk.Core.Domain.Actor;
+
+namespace Filminurk.Models.Actors
+{
+    public class ActorsIndexViewModel
+    {
+        public Guid ActorID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        List<string>? NickName { get; set; }
+        public Guid PortraitID { get; set; }
+
+        public int? Age { get; set; }
+        public string? Gender { get; set; }
+        public Child? Children { get; set; }
+    }
+}
