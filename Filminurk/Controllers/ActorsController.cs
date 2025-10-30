3218546f6b9650bd2e1d1a3db8f9c6b82928c010
Opilane TTHK <opilane@tthk.ee>
2025-10-30 11:33:19 +0200

    index

---
+++ Filminurk/Controllers/ActorsController.cs
@@ -0,0 +1,23 @@
+﻿using Filminurk.Core.ServiceInterface1;
+using Filminurk.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Filminurk.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly FilminurkTARpe24Context _context;
+        private readonly IMovieServices _movieServices;
+        private readonly IFilesServices _filesServices;
+        public ActorsController(FilminurkTARpe24Context context, IMovieServices movieServices, IFilesServices filesServices)
+        {
+            _context = context;
+            _movieServices = movieServices;
+            _filesServices = filesServices;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
